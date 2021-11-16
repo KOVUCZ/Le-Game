7c2ec314e0ced3b697a2e9de384b1651750ff79a
Adam Hrdý <Hrdy.Adam@stud.vassboskovice.cz>
2021-11-16 09:21:11 +0100

    Funkční verze, hratelná

--- Le Game/Program.cs
+++ Le Game/Program.cs
@@ -8,7 +8,9 @@ namespace Le_Game
         {
             Console.WriteLine("Le Game");
             Random random = new Random();
+            bool neuhodl = true;
             int nahodneCislo;
+            int hadaneCislo;
 
             nahodneCislo = random.Next(1, 101);
 
@@ -16,6 +18,26 @@ namespace Le_Game
             Console.WriteLine("-------------------------");
             Console.WriteLine("Myslím si číslo od 1 do 100");
 
+            while (neuhodl)
+            {
+                Console.WriteLine("Vymysli si náhodné číslo od 1 do 100");
+                hadaneCislo = int.Parse(Console.ReadLine());
+                if(hadaneCislo > nahodneCislo)
+                {
+                    Console.WriteLine("To je moc");
+                }
+                else if (hadaneCislo < nahodneCislo)
+                {
+                    Console.WriteLine("To je málo");
+                }
+                else
+                {
+                    Console.WriteLine("YAYYY UHÁDL JSI! :D");
+                    neuhodl = false;
+                }
+            }
+            
+            
             Console.ReadLine();
 
             
